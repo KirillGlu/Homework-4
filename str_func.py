89590e5e32bbc2fde13ad754e38021722bd6842c
Kirill Glushankov <kiglu7@gmail.com>
2023-06-03 15:15:20 +0500

    new func title_txt + docstring firs func

--- str_func.py
+++ str_func.py
@@ -1,3 +1,7 @@
 def upper_txt(a):
+    """Увеличивает регистр текста"""
     return print(a.upper())
 
+def title_txt(a):
+    """Увеличивает регистр первой буквы текста"""
+    return print(a.title())
